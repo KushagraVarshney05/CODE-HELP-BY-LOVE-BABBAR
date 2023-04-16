698821ab6b21434fe4dd1f00198d3565a95737e5
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-04-16 23:00:42 +0530

    allocate books

---
+++ Allocate_Books.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+bool ispossible(vector<int> nums, int n,int m,int mid)
+{
+    int sum=0;
+    int max_sum=mid;
+    int student=1;
+    for(int i=0;i<n;i++)
+    {
+        if(sum+nums[i]<=max_sum)
+        {
+            sum+=nums[i];
+        }
+        else{
+            student++;
+            if(student>m||nums[i]>max_sum)
+            {
+                return false;
+            }
+            sum=nums[i];
+        }
+    }
+    return true;
+}
+int allocateBooks(vector<int> arr, int n, int m) {
+    int sum=accumulate(arr.begin(),arr.end(),0);
+    int start=sum/m;
+    int end=sum;
+    int mid=start+(end-start)/2;
+    int ans=-1;
+    
+    while(start<=end)
+    {
+        if(ispossible(arr,n,m,mid))
+        {
+            ans=mid;
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+}
