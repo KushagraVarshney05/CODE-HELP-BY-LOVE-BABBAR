0d930f0ee8322eae6271d083889d52c811bb7dfc
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-04-19 23:00:01 +0530

    lecture 16

---
+++ string.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char arr[5]={'a','b','c','\0','d'};
+    string str="abchghd";
+    str[5]='\0';
+    cout<<arr<<endl;
+    cout<<str<<endl;
+    return 0;
+
+}
