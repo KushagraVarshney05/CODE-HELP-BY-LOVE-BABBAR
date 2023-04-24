cca953a729a4ef76a13687880a66ba1ecf6a9b55
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-04-24 09:38:19 +0530

    String Compression

---
+++ String Compression.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int count=0;
+        int i=0;
+        char temp=chars[0];
+        int ans=0;
+        while(i<chars.size())
+        {
+            // cout<<i<<endl;
+            if(chars[i]==temp)
+            {
+                count++;
+                i++;
+            }
+            else
+            {
+                
+                chars[ans]=temp;
+                ans++;
+               
+                if(count>1)
+                {
+                    string c=to_string(count);
+                    for(int j=0;j<c.length();j++)
+                    {
+                        chars[ans]=c[j];
+                        ans++;
+                    }
+                }
+                count=0;
+                temp=chars[i];
+
+            }
+                
+        }
+                chars[ans]=chars[chars.size()-1];
+                ans++;
+                if(count>1)
+                {
+                    string c=to_string(count);
+                    for(int j=0;j<c.length();j++)
+                    {
+                        chars[ans]=c[j];
+                        ans++;
+                    }
+                }
+
+        return ans;
+    }
+};
--- string.cpp
+++ string.cpp
@@ -6,6 +6,9 @@ int main()
     char arr[5]={'a','b','c','\0','d'};
     string str="abchghd";
     str[5]='\0';
+    char c='12';
+    c++;
+    cout<<c<<endl;
     cout<<arr<<endl;
     cout<<str<<endl;
     return 0;
