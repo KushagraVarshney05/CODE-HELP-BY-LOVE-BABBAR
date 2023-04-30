052479114dcae5f16312a040ef87b8883d90e4f3
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-04-30 12:15:44 +0530

    Rotaate Image

---
+++ sjdjh.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> arr{-13,-11,1,2,5,9};
+    for(int i=0;i<arr.size();i++)
+    {
+        if(arr[i]%2==0)
+        {
+            cout<<arr[i]<<endl;
+        }
+    }
+
+}
