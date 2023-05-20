0c06f9f99856ce0beed5fce5fcae3a5f636dc125
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-05-20 06:00:08 +0530

    merge sort

---
+++ recurssion/merge_sort.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+void merge(vector<int> &arr, int s, int e) {
+
+    int mid = (s+e)/2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    //copy values
+    int mainArrayIndex = s;
+    for(int i=0; i<len1; i++) {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid+1;
+    for(int i=0; i<len2; i++) {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    //merge 2 sorted arrays     
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while(index1 < len1 && index2 < len2) {
+        if(first[index1] < second[index2]) {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else{
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }   
+
+    while(index1 < len1) {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while(index2 < len2 ) {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    delete []first;
+    delete []second;
+
+}
+void mergesort(vector<int>& nums,int lb,int ub)
+{
+    if(lb<ub)
+    {
+        int mid=lb+(ub-lb)/2;
+        mergesort(nums,lb,mid);
+        mergesort(nums,mid+1,ub);
+        merge(nums,lb,ub);
+    }
+}
+    vector<int> sortArray(vector<int>& nums) {
+        int ub=nums.size()-1;
+        int lb=0;
+        mergesort(nums,lb,ub);
+        return nums;
+    }
+};
